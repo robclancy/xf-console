334f0b5d5ea171b5125b04c5651f38a6ee48e4df
Robbo- <robert@mercenarydesign.net>
2013-01-11 03:27:53 +1100

    grrrr

---
+++ src/start.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Console\Application;
+
+$application = new Application('XenForo Developer', '0.1');
+
+// done like this incase we want to add more logic here
+
+return $application;
