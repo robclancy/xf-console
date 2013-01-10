7ba53158ae92c29fd2aa3d8cb61491fa02e94d8b
Robbo- <robert@mercenarydesign.net>
2013-01-11 03:59:02 +1100

    console now initializes XenForo if it can find it

--- src/BigElephant/XfConsole/Application.php
+++ src/BigElephant/XfConsole/Application.php
@@ -2,14 +2,47 @@
 
 class Application extends \Symfony\Component\Console\Application {
 
+	protected $xfLib;
+
 	/**
 	 * Start a new Console application.
-	 * Done here like this incase of more logic in future.
 	 *
 	 * @return BigElephant\XF-Console\Application
 	 */
 	public static function start()
 	{
 		return require __DIR__.'/../../start.php';
+	}
+
+	public function detectXenForo()
+	{
+		$dir = getcwd();
+
+		// Find the library directory
+		// TODO: crawl all directories incase it isn't named library
+		$posibilities = array(
+			'/config.php' => '/',
+			'/library/config.php' => '/library/',
+			'/../library/config.php' => '/../',
+		);
+
+		foreach ($posibilities AS $try => $path)
+		{
+			if (file_exists($dir.$try))
+			{
+				$this->xfLib = $dir.$path;
+				break;
+			}
+		}
+
+		if (is_null($this->xfLib))
+		{
+			throw new \Exception('Couldn\'t locate XenForo install.');
+		}
+	}
+
+	public function getXfLibPath()
+	{
+		return $this->xfLib;
 	}
 }
--- src/start.php
+++ src/start.php
@@ -4,6 +4,23 @@ use BigElephant\XfConsole\Application;
 
 $application = new Application('XenForo Developer', '0.1');
 
-// done like this incase we want to add more logic here
+try 
+{
+	$application->detectXenForo();
+
+	require_once $application->getXfLibPath().'/XenForo/Autoloader.php';
+	XenForo_Autoloader::getInstance()->setupAutoloader($application->getXfLibPath());
+
+	XenForo_Application::setDebugMode(true);
+	XenForo_Application::initialize($application->getXfLibPath(), $application->getXfLibPath().'/../');
+
+	$dependencies = new XenForo_Dependencies_Public();
+	$dependencies->preLoadData();
+}
+catch (Exception $e)
+{
+	$application->renderException($e, new Symfony\Component\Console\Output\ConsoleOutput);
+	exit;
+}
 
 return $application;
